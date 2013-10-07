520090ab6afa9b6c2808521fb0f4c60bfac21dc4
Samuel Williams <samueltwilliams@gmail.com>
2013-10-07 19:11:33 +0100

    continued work on cover section
    almost there

--- src/sketch/core.cljs
+++ src/sketch/core.cljs
@@ -11,42 +11,12 @@
     [c2.util :refer [bind! pp]]
     [cljs.core.async.macros :refer [go]]))
 
-;; --------------------- 
-;; Example 1
-;; a simple list
-;;
-
-(bind! ".ex-basic"  
+(bind! ".ex-basic"
   [:div
    [:ol
      [:li "wibble"]
      [:li "wobble"]
      [:li "woooo"]]])
-
-;; --------------------- 
-;; Example 3
-;; ins and outs 
-;;
-
-(defn unify-example [data]
-  (bind! ".ex-keys"
-    [:div
-      [:p (unify data 
-                 (fn [d] [:p (str "(" d ")" (.now js/Date))])
-                 :key-fn (fn [d, i] d))]]))
-
-(events/listen 
-  (dom/getElement "btn-reset") "click"
-  (fn [] 
-    (dom/removeChildren (dom/getElementByClass "ex-keys"))
-    (unify-example ["a" "b" "c"])))
-
-(events/listen 
-  (dom/getElement "btn-poke") "click"
-  #(unify-example ["d" "c" "b"]))
-
-
-(unify-example ["a" "b" "c"])
 
 ;; --------------------- 
 ;; Example 4
@@ -87,7 +57,7 @@
     [:div [:div
      [:svg.chart {:width width, :height height}
       [:g {:transform "translate(10,15)"}
-       
+      (pp ticks) 
        ;; grid line labels 
        (map (fn [tick] 
              [:text.rule {:x (scale-x tick) :dy -3 :text-anchor "middle"} tick]) ticks) 
@@ -112,17 +82,19 @@
                 :dx -3
                 :dy ".35em"} d]) data)]]]]))
 
-
+;; --------------------- 
+;; Example 5
+;; animation example 
+;;
 (defn draw-chart! [data]
-  (bind! ".ex6"
+  (bind! ".ex-moving"
     [:div [:div.chart  
-      (let [size (scale/linear :domain [0 20]
-                               :range [0 100])]
-        (unify (map-indexed (fn [i d] [i d]) data)
-          (fn [[i d]] [:div.col 
-                   {:style 
-                    {:height (str (size d) "px")
-                     :left (str (* i 21) "px")}}])))]]))
+      (let [scale-x (scale/linear :domain [0 20] :range [0 100])]
+
+        (map-indexed 
+          (fn [i d] [:div.col 
+                     {:style {:height (str (scale-x d) "px")
+                              :left (str (* i 21) "px")}}]) data))]]))
 
 
 (let [seed (range 10)]
